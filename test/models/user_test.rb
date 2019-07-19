14a787f55618395411358e5c9627b25dbc5244d9
cat2koban <minamiedamame0328@gmail.com>
2019-07-19 16:37:11 +0900

    重複するメールアドレス拒否のテスト(RED)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -42,4 +42,10 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{valid_address.inspect} shoud be valid"
     end
   end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
