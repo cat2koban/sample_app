a77c243c6da354b9bef5e14b6b8c54ce64fdbc65
cat2koban <minamiedamame0328@gmail.com>
2019-07-19 16:18:27 +0900

    メールフォーマットの検証に対するテスト(RED)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -31,15 +31,15 @@ class UserTest < ActiveSupport::TestCase
 
   test "email validation should accept valid addresses" do
     valid_addresses = %w[
-      user@example.com
-      USER@foo.COM
-      A_US-ER@foo.bar.org
-      first.last@foo.jp
-      alice+bob@baz.cn
+      user@example,com
+      user_at_foo.org
+      user.name@example.
+      foo@bar_baz.com
+      foo@bar+baz.com
     ]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
-      assert @user.valid?, "#{valid_address.inspect} shoud be valid"
+      assert_not @user.valid?, "#{valid_address.inspect} shoud be valid"
     end
   end
 end
