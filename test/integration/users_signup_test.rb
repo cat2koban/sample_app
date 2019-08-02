e49bf94e648ad0c2e2a74179ffa972b829991f8b
cat2koban <minamiedamame0328@gmail.com>
2019-08-02 16:10:53 +0900

    /signup へのアクションを持つ form の存在テストを追加

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,5 +16,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.alert'
+    assert_select 'form[action="/signup"]'
   end
 end
