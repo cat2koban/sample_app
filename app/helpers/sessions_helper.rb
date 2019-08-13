bfebf3468da1482b839bc732e7abf47562214d3b
cat2koban <minamiedamame0328@gmail.com>
2019-08-13 10:17:33 +0900

    ユーザーのセッション永続化処理の追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,13 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent.[:remember_token] = user.remember_token
+  end
+
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
