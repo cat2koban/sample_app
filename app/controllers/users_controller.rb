6d985921a29d3ad439e9fc1eda22b88527042eb8
cat2koban <minamiedamame0328@gmail.com>
2019-08-18 17:05:55 +0900

    users/update アクションでの初期実装追加 edit アクションでのインデント修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,17 @@ class UsersController < ApplicationController
   end
 
   def edit
-   @user = User.find(params[:id])
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功した場合に扱う
+    else
+      render 'edit'
+    end
+
   end
 
   private
