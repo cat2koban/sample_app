eb3a5554a06d05521525b33c03ea72fe009c28b6
cat2koban <minamiedamame0328@gmail.com>
2019-08-19 22:22:16 +0900

    users/index アクションのリダイレクトに対してのテストを追加(RED)

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
