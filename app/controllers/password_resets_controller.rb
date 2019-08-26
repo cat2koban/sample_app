d12cae522241788c8cd45a773598ae8846427122
cat2koban <minamiedamame0328@gmail.com>
2019-08-26 15:34:54 +0900

    パスワード再設定用のcreateアクションの作成

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,7 +1,27 @@
 class PasswordResetsController < ApplicationController
+  # GET /password_resets/new
   def new
   end
 
+  # POST /password_resets == password_resets_path
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "Email sent with password reset instructions"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "Email address not found"
+      render 'new'
+    end
+  end
+
+  # GET /password_resets/:id/edit
   def edit
+  end
+
+  # PATCH /password_resets/:id
+  def update
   end
 end
