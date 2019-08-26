de5cdd6e9ce0ac2408a5f5a949e0772268fed898
cat2koban <minamiedamame0328@gmail.com>
2019-08-26 15:54:18 +0900

    reset_digestの生成時、現在時刻を引数として渡すよう修正

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -7,7 +7,8 @@ class PasswordResetsController < ApplicationController
   def create
     @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
-      @user.create_reset_digest
+      now = Time.zone.now
+      @user.create_reset_digest(now)
       @user.send_password_reset_email
       flash[:info] = "Email sent with password reset instructions"
       redirect_to root_url
