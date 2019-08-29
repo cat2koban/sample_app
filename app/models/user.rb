1ec55913da099e09eef62fa79b1f5d3544450a4e
cat2koban <minamiedamame0328@gmail.com>
2019-08-29 22:35:44 +0900

    Userモデルにfollowingの関連付けを追加 @user.following が使えるように...！

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
       class_name: "Relationship",
      foreign_key: "follower_id",
        dependent: :destroy
+  has_many :following,
+    through: :active_relationships,
+     source: :followed
 
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
