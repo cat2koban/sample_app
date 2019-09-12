f40a1214d34fa9fb84cc68221117379069c5d904
cat2koban <minamiedamame0328@gmail.com>
2019-09-12 21:12:45 +0900

    trait を使ってグループ化

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,23 +1,29 @@
 FactoryBot.define do
-  factory :michael, class: User do
-    name                  { "Michael Example" }
-    email                 { "michael@example.com" }
-    admin                 { true }
+  factory :user do
+    name                  { "Example User" }
+    email                 { "user@example.com" }
     activated             { true }
     activated_at          { Time.zone.now }
     password              { 'password' }
-    password_digest       { User.digest('password') }
     password_confirmation { 'password' }
-  end
 
-  factory :archer, class: User do
-    name                  { "Sterling Archer" }
-    email                 { "duchess@example.gov" }
-    admin                 { true }
-    activated             { true }
-    activated_at          { Time.zone.now }
-    password              { 'password' }
-    password_digest       { User.digest('password') }
-    password_confirmation { 'password' }
+    trait :michael do
+      name                  { "Michael Example" }
+      email                 { "michael@example.com" }
+      admin                 { true }
+      activated             { true }
+      activated_at          { Time.zone.now }
+      password              { 'password' }
+      password_confirmation { 'password' }
+    end
+
+    trait :archer do
+      name                  { "Sterling Archer" }
+      email                 { "duchess@example.gov" }
+      activated             { true }
+      activated_at          { Time.zone.now }
+      password              { 'password' }
+      password_confirmation { 'password' }
+    end
   end
 end
