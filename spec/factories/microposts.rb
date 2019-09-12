a8f606ea6fda63efa50c78c160a7e10325b4beaf
cat2koban <minamiedamame0328@gmail.com>
2019-09-12 21:18:13 +0900

    micropost用のファクトリーを作成

---
+++ spec/factories/microposts.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :micropost do
+    content    { Faker::Lorem.sentence(5) }
+    association :user
+
+    trait :most_recent do
+      created_at { Time.zone.now }
+    end
+
+  end
+end
