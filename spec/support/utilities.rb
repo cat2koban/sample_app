d2c6967fd5f49bb56d9c99ba057116434b279e7d
cat2koban <minamiedamame0328@gmail.com>
2019-09-16 00:58:07 +0900

    スペック用のログインメソッドを定義

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,3 +6,12 @@ def full_title(page_title)
     "#{page_title} | #{base_title}"
   end
 end
+
+def log_in_as(user)
+  post(login_path, params:{
+    session: {
+      email: user.email,
+      password: user.password
+    }
+  })
+end
