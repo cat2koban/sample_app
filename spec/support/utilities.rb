96d3e178ceea7a5c76799f3e98b9733994781b28
cat2koban <minamiedamame0328@gmail.com>
2019-09-19 21:50:29 +0900

    capybara使用時を考慮したメソッドに修正

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -7,11 +7,21 @@ def full_title(page_title)
   end
 end
 
-def log_in_as(user)
-  post(login_path, params:{
-    session: {
-      email: user.email,
-      password: user.password
+def log_in_as(user, options={})
+  unless options[:by_capybara]
+    post login_path, params: {
+      session: {
+        email: user.email,
+        password: user.password,
+        remember_id: '1'
+      }
     }
-  })
+  else
+    visit root_path
+    click_on 'Log in'
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    check 'session_remember_me'
+    click_button 'Log in'
+  end
 end
