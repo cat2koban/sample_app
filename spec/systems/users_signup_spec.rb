6cb44e95fdfc6287c3fb660f665009b1ba6a795c
cat2koban <minamiedamame0328@gmail.com>
2019-09-20 19:13:00 +0900

    無効な登録情報のパターンを大きく修正

--- spec/systems/users_signup_spec.rb
+++ spec/systems/users_signup_spec.rb
@@ -1,29 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe 'UsersSignup', type: :system do
-  scenario '無効なユーザー情報だと登録できない' do
+  before do
     visit signup_path
-    fill_in 'Name',         with: 'invalid user'
-    fill_in 'Email',        with: 'invalid@invalid'
-    fill_in 'Password',     with: 'foobarbaz'
-    fill_in 'Password confirmation', with: 'foobarbaz'
+  end
+
+  scenario '無効なユーザー情報だと登録できない' do
+    fill_in 'Name', with: 'a'*51
     click_button 'Create my account'
-    expect(page).to have_css('div#error_explanation')
-    have_text?('Emails is invalid')
-    have_text?('Password confirmation doesn\'t match Password')
+    expect(page).to have_content 'Name is too long (maximum is 50 characters)'
+    fill_in 'Email', with: 'invalid@invalid'
+    click_button 'Create my account'
+    expect(page).to have_content 'Email is invalid'
+    expect(page).to have_content 'Password can\'t be blank'
+    fill_in 'Password', with: 'foo'
+    fill_in 'Password confirmation', with: 'foo'
+    click_button 'Create my account'
+    expect(page).to have_content 'Password is too short (minimum is 6 characters)'
+    fill_in 'Password', with: 'foobarbaz'
+    fill_in 'Password confirmation', with: 'inva1iddddd'
+    click_button 'Create my account'
+    expect(page).to have_content 'Password confirmation doesn\'t match Password'
   end
 
   scenario '有効なユーザー情報だと登録できる' do
-    visit signup_path
     expect{
-      fill_in 'Name',         with: 'valid user'
-      fill_in 'Email',        with: 'thisUseris@invalid.com'
-      fill_in 'Password',     with: 'va1idPassw0rd!'
+      fill_in 'Name',     with: 'valid user'
+      fill_in 'Email',    with: 'thisUseris@invalid.com'
+      fill_in 'Password', with: 'va1idPassw0rd!'
       fill_in 'Password confirmation', with: 'va1idPassw0rd!'
       click_button 'Create my account'
     }.to change{ ActionMailer::Base.deliveries.size }.from(0).to(1)
     expect(current_path).to eq root_path
-    have_text?('Please check your email to activate your account.')
+    expect(page).to have_content 'Please check your email to activate your account.'
     user = User.find_by(email: 'thisuseris@invalid.com')
 
     activation_url = ActionMailer::Base.deliveries.last.body.encoded.match(
