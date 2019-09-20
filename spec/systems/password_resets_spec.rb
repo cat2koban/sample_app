be5d15c9a3f550ee0b9b56af0870867457010184
cat2koban <minamiedamame0328@gmail.com>
2019-09-21 02:30:24 +0900

    scenarioの粒度を小さく，letを使った書き方に修正

--- spec/systems/password_resets_spec.rb
+++ spec/systems/password_resets_spec.rb
@@ -1,61 +1,87 @@
 require 'rails_helper'
 
 RSpec.describe 'PasswordResets', type: :system do
-  scenario 'Passwordを忘れたので再設定する' do
-    user = create(:user)
-    visit new_password_reset_path
-
-    expect{
-      fill_in 'Email', with: 'invalid@invalid.com'
-      click_button 'Submit'
-    }.to_not change{ ActionMailer::Base.deliveries.size }
-    expect(page).to have_selector('div.alert-danger')
-    have_text?('Email address not found')
-
-    expect{
-      fill_in 'Email', with: user.email
-      click_button 'Submit'
-    }.to change{ ActionMailer::Base.deliveries.size }.from(0).to(1)
-
+  let(:user)      { create(:user) }
+  let(:send_mail) {
+    fill_in 'Email', with: user.email
+    click_button 'Submit'
+  }
+  let(:reset_url) {
     # ActionMailerが送信した最後のメールのreset_url部分を取得して
     # ポート番号をCapybara.current_session.server.portで置換している
-    reset_url = ActionMailer::Base.deliveries.last.body.encoded.match(
+    ActionMailer::Base.deliveries.last.body.encoded.match(
       "^http.*?#{CGI.escape(user.email)}"
     ).to_s.sub(
       /3000/,
       Capybara.current_session.server.port.to_s
     )
+  }
+  before do
+    visit new_password_reset_path
+  end
+
+  scenario 'Email が存在しないと flash を表示' do
+    expect{
+      fill_in 'Email', with: 'invalid@invalid.com'
+      click_button 'Submit'
+    }.to_not change{ ActionMailer::Base.deliveries.size }
+    expect(page).to have_content('Email address not found')
+  end
+
+  scenario 'Email が正しいと，メールが送信される' do
+    expect{
+      fill_in 'Email', with: user.email
+      click_button 'Submit'
+    }.to change{ ActionMailer::Base.deliveries.size }.from(0).to(1)
+  end
+
+  scenario 'アクティベイトされていないと，root_pathにリダイレクトする' do
+    send_mail
     user.toggle!(:activated)
     visit reset_url
     expect(current_path).to eq root_path
+  end
 
+  scenario 'reset_url が正しくないと, root_pathにリダイレクト' do
+    send_mail
     user.toggle!(:activated)
     visit reset_url.sub(/email.+/, "")
     expect(current_path).to eq root_path
+  end
 
+  scenario '無効なパスワードの場合，フラッシュが表示' do
+    send_mail
     visit reset_url
     fill_in 'Password',     with: 'piyo'
     fill_in 'Confirmation', with: 'piyo'
     click_on 'Update password'
-    have_selector('div#error_explanation')
-    have_text?('Password is too short (minimum is 6 characters)')
+    expect(page).to have_content('Password is too short (minimum is 6 characters)')
+  end
 
+  scenario '無効なパスワードの組み合わせの場合，フラッシュが表示' do
+    send_mail
+    visit reset_url
     fill_in 'Password',     with: 'cat2kobanisAwesome'
     fill_in 'Confirmation', with: 'piyopiyo!'
     click_on 'Update password'
-    have_selector('div#error_explanation')
-    have_text?('Password confirmation doesn\'t match Password')
+    expect(page).to have_content('Password confirmation doesn\'t match Password')
+  end
 
-    fill_in 'Password',     with: 'cat2kobanisAwesome'
-    fill_in 'Confirmation', with: 'cat2kobanisAwesome'
+  scenario '再設定したパスワードでログインできる。' do
+    user
+    send_mail
+    visit reset_url
+    new_password = 'cat2kobanisAwesome'
+    fill_in 'Password',     with: new_password
+    fill_in 'Confirmation', with: new_password
     click_on 'Update password'
     expect(current_path).to eq user_path(user.reload)
     expect(user.reset_digest).to be_nil
-
     log_out
     click_on 'Log in'
     fill_in 'Email',    with: user.email
-    fill_in 'Password', with: 'cat2kobanisAwesome'
+    fill_in 'Password', with: new_password + "hoge"
     click_button 'Log in'
+    expect(page).to_not have_css('div#error_explanation')
   end
 end
