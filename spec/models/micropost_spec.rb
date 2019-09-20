b257f74e08fb8cfdd375268b9e44447bcdc4c289
cat2koban <minamiedamame0328@gmail.com>
2019-09-21 02:28:39 +0900

    letを使った書き方に修正

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,34 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Micropost, type: :model do
-  let(:user) { create(:user, :michael) }
-  before do
-    @micropost = create(:micropost, user: user)
-  end
+  let(:user)      { create(:user, :michael) }
+  let(:micropost) { create(:micropost, user: user) }
 
   context "有効なマイクロポストを作成した時" do
     it "validとなる" do
-      expect(@micropost).to be_valid
+      expect(micropost).to be_valid
     end
   end
 
   context "無効なマイクロポストを作成した時" do
     it "user_idが無いと，invalidとなる" do
-      @micropost.user_id = nil
-      @micropost.valid?
-      expect(@micropost.errors[:user_id]).to include("can't be blank")
+      micropost.user_id = nil
+      micropost.valid?
+      expect(micropost.errors[:user_id]).to include('can\'t be blank')
     end
 
     it "contentが無いと，invalidとなる" do
-      @micropost.content = "  "
-      @micropost.valid?
-      expect(@micropost.errors[:content]).to include("can't be blank")
+      micropost.content = "  "
+      micropost.valid?
+      expect(micropost.errors[:content]).to include('can\'t be blank')
     end
 
     it "contentが140文字以上だと，invalidとなる" do
-      @micropost.content = "a" * 141
-      @micropost.valid?
-      expect(@micropost.errors[:content]).to include("is too long (maximum is 140 characters)")
+      micropost.content = "a" * 141
+      micropost.valid?
+      expect(micropost.errors[:content]).to include('is too long (maximum is 140 characters)')
     end
   end
 
